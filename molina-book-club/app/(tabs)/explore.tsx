45662c29ae0a666ab1984ccde99ad0c39bbd6d6f
Maria Alejandra Molina <mmolina6@villanova.edu>
2024-12-03 22:21:54 -0500

    Worked on Search Box and started api call

--- molina-book-club/app/(tabs)/explore.tsx
+++ molina-book-club/app/(tabs)/explore.tsx
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TextInput, Button, FlatList} from 'react-native';
+import { SearchBar } from 'react-native-screens';
+import { SafeAreaView } from 'react-native';
 
-const SearchRecs = () => { 
-  const [query, setQuery] = useState(''); 
-
-}
+const url_open_lib = 'https://openlibrary.org/search.json?q=${query}'
 
 export default function Tab() {
+  const [searchQuery, setSearchQuery] = useState(''); 
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]); 
+  const [error, setError] = useState(null); 
+  const [fullData, setFullData] = useState([]); 
+
+useEffect(() => { 
+  setLoading(true); 
+  //fetchBooks(url_open_lib); 
+}, []);
+
+
+  const handleSearch = (query: React.SetStateAction<string>) => 
+    setSearchQuery(query); 
+
   return (
-    <View style={styles.container}>
-      <Text>Tab [Home|Explore|Profile]</Text>
-    </View>
+   <SafeAreaView style = {{flex:1, marginHorizontal: 20}}> 
+    <TextInput 
+      placeholder='Search' 
+      clearButtonMode = 'always'
+      style = {styles.searchBox}
+      value = {searchQuery}
+      onChangeText={(query) => handleSearch(query)}
+    /> 
+
+   </SafeAreaView>
   );
 }
 
@@ -20,4 +41,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  searchBox: { 
+    paddingHorizontal:20, 
+        paddingVertical:10, 
+        borderColor: 'skyblue',
+        borderWidth: 1,
+        borderRadius: 8,
+  }
 });
