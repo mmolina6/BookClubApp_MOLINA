063b9828ed2653badc863557839bf38a8a8ff42e
Maria Alejandra Molina <mmolina6@villanova.edu>
2025-07-28 23:20:12 -0400

    Updates to home

--- molina-book-club/app/(tabs)/index.tsx
+++ molina-book-club/app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
-import { View, Text, StyleSheet } from 'react-native';
-//import {firestore, auth} from './firebaseConfig'; 
-//import {firestore, auth} from '/Users/alemolina/Desktop/Grand Challenge/git-hub_repo/BookClubApp_MOLINA/molina-book-club/firebaseConfig.js'
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function Tab() {
   return (
     <View style={styles.container}>
-      <Text>Tab [Home|Explore|Profile]</Text>
+      <Text style={styles.welcomeText}>Welcome to Novel Net!</Text>
+      <TouchableOpacity style={styles.button} onPress={() => console.log('Creating club')}>
+        <Text style={styles.buttonText}>Create a Book Club</Text>
+      </TouchableOpacity>
+       <TouchableOpacity style={styles.button} onPress={() => console.log('Joining club')}>
+        <Text style={styles.buttonText}>  Join a Book Club  </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,7 +18,30 @@ export default function Tab() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    backgroundColor: '#FAFAFA',
+    padding: 20,
+    paddingTop: 80,
+  },
+  welcomeText: {
+    fontSize: 28,
+    fontWeight: '600',
+    color: '#333',
+    textAlign: 'center',
+    letterSpacing: 1,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#4E93BD',
+    paddingVertical: 30,
+    paddingHorizontal: 48,
+    borderRadius: 8,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '500',
   },
 });
